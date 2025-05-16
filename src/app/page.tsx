4e98590f3335a666b7330eb161deb075624569a3
seondal <sseondal@gmail.com>
2025-05-16 16:52:23 +0900

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <main className="p-4">
-      <h1>Noviwi | 노션 비디오 위젯</h1>
-      <p>동영상 자동 재생 노션 위젯</p>
-      <article>
-        <header> Autoplay Video Widget for Notion</header>
-        <Link href="https://peridot-riddle-679.notion.site/noviwi?pvs=4">
-          ➡️ 홈페이지 Homepage
-        </Link>
-      </article>
-    </main>
-  );
+  redirect("https://peridot-riddle-679.notion.site/noviwi?pvs=4");
 }
