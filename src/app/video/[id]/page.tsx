bd55985e1139834c3f8e9ffd5c4ee3055820f6a5
seondal <sseondal@gmail.com>
2025-09-04 19:57:13 +0900

    rollback

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -21,16 +21,16 @@ export default async function Page({
   const videoSrc = `/assets/${id}.mp4`;
   const videoExists = fs.existsSync(videoSrc);
 
-  if (!videoExists) {
-    return (
-      <a href="/" target="_blank" rel="noopener noreferrer">
-        <div className="flex flex-col w-full h-full items-center justify-center">
-          <h1>Noviwi</h1>
-          <h5>노션 비디오 위젯 제작</h5>
-        </div>
-      </a>
-    );
-  }
+  // if (!videoExists) {
+  //   return (
+  //     <a href="/" target="_blank" rel="noopener noreferrer">
+  //       <div className="flex flex-col w-full h-full items-center justify-center">
+  //         <h1>Noviwi</h1>
+  //         <h5>노션 비디오 위젯 제작</h5>
+  //       </div>
+  //     </a>
+  //   );
+  // }
 
   return (
     <a href="/" target="_blank" rel="noopener noreferrer">
